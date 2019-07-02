9c28c408e0c1c280f64f8acfba18ba9e48fdecba
Robert Koeninger <rkoeninger@att.net>
2019-07-02 17:25:17 -0400

    Added go version of hanoi

---
+++ src/day001/hanoi.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func hanoi_recur(height int, from string, to string, through string) []string {
+	if height > 0 {
+		return append(
+			hanoi_recur(height-1, from, through, to),
+			append(
+				[]string{from + " => " + to},
+				hanoi_recur(height-1, through, to, from)...)...)
+	}
+	return []string{}
+}
+
+func hanoi(height int) []string {
+	return hanoi_recur(height, "left", "right", "center")
+}
+
+func show(moves []string) {
+	for i := 0; i < len(moves); i++ {
+		fmt.Println(moves[i])
+	}
+	fmt.Println()
+}
+
+func main() {
+	show(hanoi(1))
+	show(hanoi(2))
+	show(hanoi(3))
+	show(hanoi(4))
+}
